16598983d268718803c12e306f7f4f71326161bb
Takashi Mori <kanetugu2015@gmail.com>
2021-09-06 16:30:23 +0900

    redundant code is deleted

--- create-json.py
+++ create-json.py
@@ -5,19 +5,10 @@ filepath = 'road_map.json'
         
 root = RoadRoot()
 
-entry = RoadEntry('road2', 0.0, None)
-root.add(entry)
-
-entry = RoadEntry('road1', 0.0, '+z')
-root.add(entry)
-
-entry = RoadEntry('road2', 180.0, '+z')
-root.add(entry)
-
-entry = RoadEntry('road1', 90.0, '+x')
-root.add(entry)
-
-entry = RoadEntry('road2', 270.0, '+x')
-root.add(entry)
+root.add(RoadEntry('road2', 0.0,    None))
+root.add(RoadEntry('road1', 0.0,    '+z'))
+root.add(RoadEntry('road2', 180.0,  '+z'))
+root.add(RoadEntry('road1', 90.0,   '+x'))
+root.add(RoadEntry('road2', 270.0,  '+x'))
 
 root.dump(filepath)
